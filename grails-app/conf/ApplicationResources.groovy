9695500a9ee951f273e49f4149199c22878729a5
Kevin Brown <silentkevin@gmail.com>
2014-01-20 19:48:41 -0500

    checkpoint

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -1,5 +1,17 @@
 modules = {
     application {
         resource url:'js/application.js'
+
+        dependsOn 'angular', 'angular-route', 'angular-cookies'
+
+        resource url: 'js/app.js'
+
+        //resource url: 'js/services/'
+
+        //resource url: 'js/controllers/'
+
+        //resource url: 'js/filters/'
+
+        //resource url: 'js/directives/'
     }
 }
